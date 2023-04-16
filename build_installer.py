0bb5767925bc8a51e0c6bd93231f7346d9db40ff
Robert Almendarez <r-almendarez@ti.com>
2023-04-15 22:43:46 -0500

    Added the DPAM VIF Spec to the GUI Help menu, where, when selected, it will trigger opening the spec using the OS's default PDF application

--- build_installer.py
+++ build_installer.py
@@ -6,28 +6,52 @@
 # IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
 # PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
 ######################################################
-import PyInstaller.__main__
+import glob
 import os
+
+import PyInstaller.__main__
+
+from dpamvifgenerator.utility import get_asset_file_path
+
+
+def generate_pdf_args():
+    pdfs = glob.glob(get_asset_file_path("assets", "DPAM_VIF_Spec*.pdf"))
+    # Add PDF files to build data
+    for pdf in pdfs:
+        yield "--add-data"
+        yield pdf + os.pathsep + "assets"
+
 
 PyInstaller.__main__.run(
     [
+        # Script Main
         "./dpamvifgenerator/main.py",
         "--name",
         "dpamvifgenerator",
         "--onefile",
         "--windowed",
         "--noconfirm",
+        # GUI UI Files
         "--add-data",
-        os.path.join("dpamvifgenerator", "uifiles", "mainwindow.ui") + os.pathsep + os.path.join("dpamvifgenerator","uifiles"),
+        os.path.join("dpamvifgenerator", "uifiles", "mainwindow.ui")
+        + os.pathsep
+        + os.path.join("dpamvifgenerator", "uifiles"),
         "--add-data",
-        os.path.join("dpamvifgenerator", "uifiles", "about.ui") + os.pathsep + os.path.join("dpamvifgenerator", "uifiles"),
+        os.path.join("dpamvifgenerator", "uifiles", "about.ui")
+        + os.pathsep
+        + os.path.join("dpamvifgenerator", "uifiles"),
+        # GUI Splash and Icons
         "--add-data",
         os.path.join("assets", "vesa_logo_dark.png") + os.pathsep + "assets",
         "--add-data",
         os.path.join("assets", "vesa_logo_light.png") + os.pathsep + "assets",
         "--add-data",
         os.path.join("assets", "displayport_icon.ico") + os.pathsep + "assets",
+        # DPAM VIF Spec PDFs
+        *list(generate_pdf_args()),
+        # Function Imports
         "--hidden-import=ctypes",
+        # Icon
         "--icon=./assets/displayport_icon.ico",
     ]
 )
